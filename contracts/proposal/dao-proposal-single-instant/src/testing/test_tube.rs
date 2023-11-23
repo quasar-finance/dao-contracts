88e3fd5aa1443b1b5b7bdac498d510136d85ed63
magiodev.eth <31893902+magiodev@users.noreply.github.com>
2023-11-23 14:35:09 +0100

    more test tube

--- contracts/proposal/dao-proposal-single-instant/src/testing/test_tube.rs
+++ contracts/proposal/dao-proposal-single-instant/src/testing/test_tube.rs
@@ -1,36 +1,22 @@
 #[cfg(test)]
 pub mod test_tube {
-    use cosmwasm_std::Addr;
+    use std::collections::HashMap;
+
     use cosmwasm_std::Coin;
-    use cosmwasm_std::CosmosMsg;
     use osmosis_test_tube::Account;
     use osmosis_test_tube::{Module, OsmosisTestApp, SigningAccount, Wasm};
 
+    use crate::msg::InstantiateMsg;
     use crate::msg::SingleChoiceInstantProposeMsg;
     use crate::state::VoteSignature;
     // use cosmrs::bip32::secp256k1::ecdsa::signature::
     const INITIAL_BALANCE_AMOUNT: u128 = 340282366920938463463374607431768211455u128;
 
-    pub fn default_init() -> (
-        OsmosisTestApp,
-        Addr,
-        Addr,
-        SigningAccount,
-        Vec<SigningAccount>,
-    ) {
-        init_test_contract(
-            "./test-tube-build/wasm32-unknown-unknown/release/cl_vault.wasm",
-            10,
-        )
-    }
-
-    pub fn init_test_contract(
-        filename: &str,
+    pub fn test_init(
         voters_number: u32,
     ) -> (
         OsmosisTestApp,
-        Addr,
-        Addr,
+        HashMap<&'static str, &'static str>,
         SigningAccount,
         Vec<SigningAccount>,
     ) {
@@ -43,6 +29,7 @@ pub mod test_tube {
             .init_account(&[Coin::new(INITIAL_BALANCE_AMOUNT, "uosmo")])
             .unwrap();
 
+        // Create voters accounts
         let mut voters: Vec<SigningAccount> = vec![];
         for _ in 0..voters_number {
             voters.push(
@@ -51,55 +38,88 @@ pub mod test_tube {
             )
         }
 
-        // TODO: Take store and instantiate steps from testing::instantiate::instantiate_with_cw4_groups_governance() unit test
-        // TODO: Dao core
-        // TODO: Proposal module
-        // TODO: External dependencies
-        // TODO: ...
+        // Contracts to store and instantiate
+        let init_msg1 = InstantiateMsg {
+            threshold: (),
+            max_voting_period: (),
+            min_voting_period: (),
+            only_members_execute: (),
+            allow_revoting: (),
+            pre_propose_info: (),
+            close_proposal_on_execution_failure: (),
+        };
+        let init_msg2 = InstantiateMsg {
+            threshold: (),
+            max_voting_period: (),
+            min_voting_period: (),
+            only_members_execute: (),
+            allow_revoting: (),
+            pre_propose_info: (),
+            close_proposal_on_execution_failure: (),
+        };
 
-        // Load compiled wasm bytecode
-        let wasm_byte_code = std::fs::read(filename).unwrap();
-        let code_id = wasm
-            .store_code(&wasm_byte_code, None, &admin)
-            .unwrap()
-            .data
-            .code_id;
+        let contracts_setup: Vec<(&str, &str, InstantiateMsg)> = vec![
+            (
+                "dao-dao-core",
+                "./test-tube-build/wasm32-unknown-unknown/release/dao_dao_core.wasm",
+                init_msg1,
+            ),
+            (
+                "dao-proposal-single-instant",
+                "./test-tube-build/wasm32-unknown-unknown/release/dao_proposal_single_instant.wasm",
+                init_msg2,
+            ),
+        ];
 
-        // Instantiate dao contracts
-        let core_contract = wasm
-            .instantiate(
-                code_id,
-                &InstantiateMsg {
-                    // TODO: inst message
-                },
-                Some(admin.address().as_str()),
-                Some("dao-dao-core"),
-                vec![].as_ref(),
-                &admin,
-            )
-            .unwrap();
+        // [START] - (relevant: testing::instantiate::instantiate_with_cw4_groups_governance() unit test)
 
-        // Instantiate dao contracts
-        let proposal_contract = wasm
-            .instantiate(
-                code_id,
-                &InstantiateMsg {
-                    // TODO: inst message
-                },
-                Some(admin.address().as_str()),
-                Some("dao-proposal-single-instant"),
-                vec![].as_ref(),
-                &admin,
-            )
-            .unwrap();
+        // Store contracts and declare an array of tuples
+        let code_ids: Vec<(&str, u64, InstantiateMsg)> = contracts_setup
+            .iter()
+            .map(|&(contract_name, file_name, inst_msg)| {
+                let wasm_byte_code = std::fs::read(file_name)
+                    .expect(format!("Failed to read file: {}", file_name).as_str());
 
-        // TODO: Ensure we created memberships as:
+                let code_id = wasm
+                    .store_code(&wasm_byte_code, None, &admin)
+                    .expect("Failed to store code")
+                    .data
+                    .code_id;
+
+                (contract_name, code_id, inst_msg)
+            })
+            .collect();
+
+        // HashMap to store contract names and their addresses
+        let mut contracts: HashMap<&str, &str> = HashMap::new();
+
+        // Final iteration to instantiate contracts and populate the HashMap
+        for contract in code_ids {
+            let instantiated_contract = wasm
+                .instantiate(
+                    contract.1,
+                    &contract.2,
+                    Some(admin.address().as_str()),
+                    Some(contract.0),
+                    vec![].as_ref(),
+                    &admin,
+                )
+                .expect("Failed to instantiate contract");
+
+                contracts.insert(
+                contract.0,
+                instantiated_contract.data.address.as_str(),
+            );
+        }
+
+        // TODO: Ensure memberships are created as specified
+        // For example:
         // - Proposers: admin, weight 0
         // - Voters: voters foreach, weight 1
+
         (
             app,
-            Addr::unchecked(core_contract.data.address),
-            Addr::unchecked(proposal_contract.data.address), // TODO: change this with proposal contract
+            contracts,
             admin,
             voters,
         )
@@ -107,12 +127,12 @@ pub mod test_tube {
 
     #[test]
     #[ignore]
-    fn default_init_works() {
-        let (app, _core_address, proposal_address, admin, voters) = default_init();
+    fn test_dao_proposal_single_instant() {
+        let (app, contracts, admin, voters) = test_init(3);
         let wasm = Wasm::new(&app);
 
         // TODO: Mock signatures taking voter.publickey to recover the sig
-        let vote_signatures: Vec<VoteSignature> = vec![];
+        let mut vote_signatures: Vec<VoteSignature> = vec![];
         for voter in voters {
             // Dummy message
             let msg: &[u8] = "Hello World!".as_bytes();
@@ -124,18 +144,20 @@ pub mod test_tube {
         }
 
         // Execute execute_propose (proposal, voting and execution in one single workflow)
-        let execute_propose_resp = wasm.execute(
-            &proposal_address.to_string(),
-            &SingleChoiceInstantProposeMsg {
-                title: "Title".to_string(),
-                description: "Description".to_string(),
-                msgs: vec![], // TODO: Mock a simple bank transfer that in prod will be the trigger exec to the middleware contract
-                proposer: Some(admin.address()),
-                votes: vec![],
-            },
-            &vec![],
-            &admin
-        ).unwrap();
+        let execute_propose_resp = wasm
+            .execute(
+                contracts.get("dao-proposal-single-instant").unwrap(),
+                &SingleChoiceInstantProposeMsg {
+                    title: "Title".to_string(),
+                    description: "Description".to_string(),
+                    msgs: vec![], // TODO: Mock a simple bank transfer that in prod will be the trigger exec to the middleware contract
+                    proposer: Some(admin.address()),
+                    votes: vec![],
+                },
+                &vec![],
+                &admin,
+            )
+            .unwrap();
 
         // TODO: Query things from contract to make assertions
         // let resp = wasm
